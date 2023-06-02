0dbbff31206881320455cb8e432bed9a6e631a8e
Junhyunny <kang3966@naver.com>
2023-06-02 13:49:15 +0900

    Add terraform RDS example

--- examples/chapter-042/route53.tf
+++ examples/chapter-042/route53.tf
@@ -3,26 +3,26 @@ resource "aws_route53_zone" "newtech-academy" {
 }
 
 resource "aws_route53_record" "server1-record" {
-    zone_id = aws_route53_zone.newtech-academy.zone_id
-    name = "server1.newtech.academy"
-    type = "A"
-    ttl = "300"
-    records = ["104.236.247.8"]
+  zone_id = aws_route53_zone.newtech-academy.zone_id
+  name    = "server1.newtech.academy"
+  type    = "A"
+  ttl     = "300"
+  records = ["104.236.247.8"]
 }
 
-resource "aws_route53_record" "www-record"{
- zone_id = aws_route53_zone.newtech-academy.zone_id
- name = "www.newtech.academy"
- type = "A"
- ttl = "300"
- records = ["104.236.247.8"]
+resource "aws_route53_record" "www-record" {
+  zone_id = aws_route53_zone.newtech-academy.zone_id
+  name    = "www.newtech.academy"
+  type    = "A"
+  ttl     = "300"
+  records = ["104.236.247.8"]
 }
 
 resource "aws_route53_record" "mail1-record" {
   zone_id = aws_route53_zone.newtech-academy.zone_id
-  name = "newtech.academy"
-  type = "MX"
-  ttl = "300"
+  name    = "newtech.academy"
+  type    = "MX"
+  ttl     = "300"
   records = [
     "1 aspmx.l.google.com.",
     "5 alt1.aspmx.l.google.com.",
